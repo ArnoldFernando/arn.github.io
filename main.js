9d54e3860c42fec03e7bda631d437c8ebcb97b13
ArnoldFernando <115635149+ArnoldFernando@users.noreply.github.com>
2023-06-10 16:48:57 +0800

    other's website

---
+++ main.js
@@ -0,0 +1,15 @@
+function showAlert() {
+  alert("Hello!");
+}
+
+function contactInfo() {
+  alert("email: others@gmail.com");
+  alert("cellphone no. 09355529021")
+}
+
+
+// dark mode
+function toggleMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
